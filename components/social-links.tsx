8a260db9730fed907fa282c163ac2528efe79e27
oman619 <faisalalanqood@gmail.com>
2024-03-03 11:01:28 +0400

    new-v

--- components/social-links.tsx
+++ components/social-links.tsx
@@ -5,13 +5,13 @@ import { faGithub, faYoutube, faLinkedin } from '@fortawesome/free-brands-svg-ic
 const SocialMediaLinks = () => {
     return (
         <div className='flex flex-row gap-4'>
-            <a href="https://github.com/KoushikJit" target="_blank" rel="noopener noreferrer">
+            <a href="https://github.com/alanqoudif" target="_blank" rel="noopener noreferrer">
                 <FontAwesomeIcon icon={faGithub} size="2x" />
             </a>
-            <a href="https://www.youtube.com/channel/UCfKRZUU78XIcQvh1_QsdQyA" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.youtube.com/channel/UCGhDVxssxWg1WNB4woa5mfQ" target="_blank" rel="noopener noreferrer">
                 <FontAwesomeIcon icon={faYoutube} size="2x" />
             </a>
-            <a href="https://www.linkedin.com/in/koushik-sarkar-iimc/" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.linkedin.com/in/faisal-al-anqoudi-33a345217/" target="_blank" rel="noopener noreferrer">
                 <FontAwesomeIcon icon={faLinkedin} size="2x" />
             </a>
         </div>
